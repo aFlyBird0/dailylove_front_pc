e5c7263495e21834a41b0531c6355df320bf1e70
BirdBirdLee <1606251360@qq.com>
2019-07-23 16:51:17 +0800

    迁移完成，目前有注册，登录，添加，删除，展示功能

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If iView is successfully added to this project, you'll see an
-        <code v-text="'<Button>'"></code>
-        below
-      </p>
-      <Button type="primary">Button</Button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img src="./assets/logo.png" />
+    <p>
+      <router-link :to="{name:'show'}">展示界面</router-link>
+      <p>路由出现了问题<p>
+    </p> -->
+    <!-- <p>
+      <Button @click="gotoShow">点我去展示界面</Button>
+      <Button @click="gotoIndex">点我返回主页面</Button>
+    </p> -->
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    gotoShow: function(){
+      this.$router.push({name:'show'});
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
